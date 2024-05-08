027cdd5aba1484cf368cfd1109c020b9c67083fa
Ilya <virus-birus@mail.ru>
2024-05-08 18:20:06 +0300

    Add numbers in main Timer

--- projectiv/js/main.js
+++ projectiv/js/main.js
@@ -19,3 +19,41 @@ Close.addEventListener('click', ()=> {
    } 
    
 })
+
+const AddTimes = document.querySelector('.AddTimes') //button
+
+//1
+const wd_timer = document.querySelector('.wd_timer')
+const wh_timer = document.querySelector('.wh_timer')
+const ws_timer = document.querySelector('.ws_timer')
+
+//2
+const wd_timerr = document.querySelector('.wd_timerr')
+const wh_timerr = document.querySelector('.wh_timerr')
+const ws_timerr = document.querySelector('.ws_timerr')
+
+
+
+
+AddTimes.addEventListener('click', () => {
+   const wd_timerrs = wd_timerr.value
+   const wh_timerrs = wh_timerr.value
+   const ws_timerrs = ws_timerr.value
+
+   wd_timer.innerText = wd_timerrs 
+   wh_timer.innerText = wh_timerrs 
+   ws_timer.innerText = ws_timerrs
+
+   conteiner_1.classList.remove('conteiner_11')
+   conteiner_2.classList.add('conteiner_22')
+    
+   if(wd_timerr.value === '' && wh_timerr.value === '' && ws_timerr.value === '') {
+    wd_timer.innerText = '00'
+    wh_timer.innerText = '00'
+    ws_timer.innerText = '00'
+   }
+
+})
+
+ 
+
